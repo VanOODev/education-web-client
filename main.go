d4c82e556b8148d658e08668f816c30a9fd3704e
MrDarkLast <stepan.sin@yandex.ru>
2023-03-18 20:27:04 +0300

    Добавил пользование через консоль

--- main.go
+++ main.go
@@ -14,14 +14,80 @@ type Storage interface {
 }
 
 func main() {
+	a := 0
+	b := 0
 	var l Storage = list.NewList()
 	var sl Storage = slice.NewSliceStorage()
-	l.Add(10)
-	l.Add(30)
-	sl.Add(20)
-	sl.Add(50)
-	//	fmt.Println(l.Get(3))
-	fmt.Println(l.String())
-	fmt.Print(sl.String())
 
+	for isTrue := true; isTrue != false; {
+		fmt.Println("1: Односвязный список")
+		fmt.Println("2: Срезы")
+		fmt.Println("0: Выйти из программы")
+		fmt.Scan(&a)
+		switch a {
+
+		case 1:
+			for isTrue1 := true; isTrue1 != false; {
+				fmt.Println("1: Добавить в элемент в лист")
+				fmt.Println("2: Удалить элемент из листа")
+				fmt.Println("3: Показать элемент")
+				fmt.Println("0: Выйти из подпрограммы")
+				fmt.Scan(&b)
+				switch b {
+
+				case 1:
+					fmt.Println("Введите значение")
+					var temp int64 = 0
+					fmt.Scan(&temp)
+					l.Add(temp)
+				case 2:
+					fmt.Println("Введите индекс")
+					var temp int64 = 0
+					fmt.Scan(&temp)
+					l.Delete(temp)
+				case 3:
+					fmt.Println("Введите индекс")
+					var temp int64 = 0
+					fmt.Scan(&temp)
+					fmt.Println(l.Get(temp))
+				case 0:
+					isTrue1 = false
+				}
+			}
+
+		case 2:
+			for isTrue1 := true; isTrue1 != false; {
+				fmt.Println("1: Добавить в элемент в срез")
+				fmt.Println("2: Удалить элемент из среза")
+				fmt.Println("3: Показать элемент")
+				fmt.Println("0: Выйти из подпрограммы")
+				fmt.Scan(&b)
+				switch b {
+
+				case 1:
+					fmt.Println("Введите значение")
+					var temp int64 = 0
+					fmt.Scan(&temp)
+					sl.Add(temp)
+				case 2:
+					fmt.Println("Введите индекс")
+					var temp int64 = 0
+					fmt.Scan(&temp)
+					sl.Delete(temp)
+				case 3:
+					fmt.Println("Введите индекс")
+					var temp int64 = 0
+					fmt.Scan(&temp)
+					fmt.Println(sl.Get(temp))
+
+				case 0:
+					isTrue1 = false
+				}
+			}
+
+		case 0:
+			isTrue = false
+		}
+	}
+	fmt.Println("До свидания!")
 }
