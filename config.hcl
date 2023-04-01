9f68dfc650ac88fb7ec2721484a113d4a01a523d
MrDarkLast <stepan.sin@yandex.ru>
2023-04-01 20:23:45 +0300

    Добавил файл конфигурации

---
+++ config.hcl
@@ -0,0 +1,11 @@
+database {
+  dbname = "mydatabase"
+  user = "postgres"
+  password = "mypassword"
+  host = "localhost"
+  port = 5432
+}
+
+storagetype {
+  storage_type = "Лист"
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/VanOODev/education-web-client/storages/list"
 	"github.com/VanOODev/education-web-client/storages/slice"
+	"github.com/hashicorp/hcl/v2/hclsimple"
 )
 
 type Storage interface {
@@ -13,81 +14,114 @@ type Storage interface {
 	String() string
 }
 
+type Config struct {
+	Database struct {
+		DBName   string `hcl:"dbname"`
+		User     string `hcl:"user"`
+		Password string `hcl:"password"`
+		Host     string `hcl:"host"`
+		Port     int    `hcl:"port"`
+	} `hcl:"database,block"`
+	StorageType struct {
+		Storage_Type string `hcl:"storage_type"`
+	} `hcl:"storagetype,block"'`
+}
+
 func main() {
-	a := 0
+	var cfg Config
+	err := hclsimple.DecodeFile("config.hcl", nil, &cfg)
+	if err != nil {
+		fmt.Printf("Ошибка загрузки файла конфигурации: %v\n", err)
+		return
+	}
 	b := 0
 	var l Storage = list.NewList()
 	var sl Storage = slice.NewSliceStorage()
 
-	for isTrue := true; isTrue != false; {
-		fmt.Println("1: Односвязный список")
-		fmt.Println("2: Срезы")
-		fmt.Println("0: Выйти из программы")
-		fmt.Scan(&a)
-		switch a {
+	switch cfg.StorageType.Storage_Type {
 
-		case 1:
-			for isTrue1 := true; isTrue1 != false; {
-				fmt.Println("1: Добавить в элемент в лист")
-				fmt.Println("2: Удалить элемент из листа")
-				fmt.Println("3: Показать элемент")
-				fmt.Println("0: Выйти из подпрограммы")
-				fmt.Scan(&b)
-				switch b {
+	case "Лист":
+		for isTrue1 := true; isTrue1 != false; {
+			fmt.Println("1: Добавить в элемент в лист")
+			fmt.Println("2: Удалить элемент из листа")
+			fmt.Println("3: Показать элемент")
+			fmt.Println("0: Выйти из подпрограммы")
+			fmt.Scan(&b)
+			switch b {
 
-				case 1:
-					fmt.Println("Введите значение")
-					var temp int64 = 0
-					fmt.Scan(&temp)
-					l.Add(temp)
-				case 2:
-					fmt.Println("Введите индекс")
-					var temp int64 = 0
-					fmt.Scan(&temp)
-					l.Delete(temp)
-				case 3:
-					fmt.Println("Введите индекс")
-					var temp int64 = 0
-					fmt.Scan(&temp)
-					fmt.Println(l.Get(temp))
-				case 0:
-					isTrue1 = false
-				}
+			case 1:
+				fmt.Println("Введите значение")
+				var temp int64 = 0
+				fmt.Scan(&temp)
+				l.Add(temp)
+			case 2:
+				fmt.Println("Введите индекс")
+				var temp int64 = 0
+				fmt.Scan(&temp)
+				l.Delete(temp)
+			case 3:
+				fmt.Println("Введите индекс")
+				var temp int64 = 0
+				fmt.Scan(&temp)
+				fmt.Println(l.Get(temp))
+			case 0:
+				isTrue1 = false
 			}
-
-		case 2:
-			for isTrue1 := true; isTrue1 != false; {
-				fmt.Println("1: Добавить в элемент в срез")
-				fmt.Println("2: Удалить элемент из среза")
-				fmt.Println("3: Показать элемент")
-				fmt.Println("0: Выйти из подпрограммы")
-				fmt.Scan(&b)
-				switch b {
-
-				case 1:
-					fmt.Println("Введите значение")
-					var temp int64 = 0
-					fmt.Scan(&temp)
-					sl.Add(temp)
-				case 2:
-					fmt.Println("Введите индекс")
-					var temp int64 = 0
-					fmt.Scan(&temp)
-					sl.Delete(temp)
-				case 3:
-					fmt.Println("Введите индекс")
-					var temp int64 = 0
-					fmt.Scan(&temp)
-					fmt.Println(sl.Get(temp))
-
-				case 0:
-					isTrue1 = false
-				}
-			}
-
-		case 0:
-			isTrue = false
 		}
+
+	case "Срез":
+		for isTrue1 := true; isTrue1 != false; {
+			fmt.Println("1: Добавить в элемент в срез")
+			fmt.Println("2: Удалить элемент из среза")
+			fmt.Println("3: Показать элемент")
+			fmt.Println("0: Выйти из подпрограммы")
+			fmt.Scan(&b)
+			switch b {
+
+			case 1:
+				fmt.Println("Введите значение")
+				var temp int64 = 0
+				fmt.Scan(&temp)
+				sl.Add(temp)
+			case 2:
+				fmt.Println("Введите индекс")
+				var temp int64 = 0
+				fmt.Scan(&temp)
+				sl.Delete(temp)
+			case 3:
+				fmt.Println("Введите индекс")
+				var temp int64 = 0
+				fmt.Scan(&temp)
+				fmt.Println(sl.Get(temp))
+
+			case 0:
+				isTrue1 = false
+			}
+		}
+	case "База данных":
+
+		for isTrue1 := true; isTrue1 != false; {
+			fmt.Println("1: Добавить в элемент в базу данных")
+			fmt.Println("2: Удалить элемент из базы данных")
+			fmt.Println("3: Показать элемент")
+			fmt.Println("0: Выйти из подпрограммы")
+			fmt.Scan(&b)
+			switch b {
+
+			case 1:
+				fmt.Println("Введите значение")
+
+			case 2:
+				fmt.Println("Введите индекс")
+
+			case 3:
+				fmt.Println("Введите индекс")
+
+			case 0:
+				isTrue1 = false
+			}
+
+		}
+		fmt.Println("До свидания!")
 	}
-	fmt.Println("До свидания!")
 }
